3f09741ab1223f3540a858c67359dee2ef945ae7
Himank Singh <110485567+Maverick-hsk@users.noreply.github.com>
2025-11-13 23:21:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/K Sized Subarray Maximum/k-sized-subarray-maximum.cpp
@@ -0,0 +1,32 @@
+class Solution {
+  public:
+    vector<int> maxOfSubarrays(vector<int>& arr, int k) {
+        int i=0;
+        int j=0;
+        int n = arr.size();
+        list<int> l;
+        vector<int>ans;
+        while(j<n){
+            while(arr[j]>l.back() && l.size()>0){
+                l.pop_back();
+            }
+            l.push_back(arr[j]);
+            
+            if(j-i+1 <k){
+                j++;
+            }
+            
+            else if(j-i+1 ==k){
+                ans.push_back(l.front());
+               if(arr[i]==l.front()){
+                   l.pop_front();
+               }
+               i++;
+               j++;
+            }
+            
+            
+        }
+        return ans;
+    }
+};
