0826ea172ca5eef4295b85dfb8adf217529a1419
Himank Singh <110485567+Maverick-hsk@users.noreply.github.com>
2026-01-28 10:54:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Stock Buy and Sell – Max one Transaction Allowed/stock-buy-and-sell-max-one-transaction-allowed.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    int maximumProfit(vector<int> &prices) {
+     int n = prices.size();
+     int mini = prices[0];
+     int profit = 0;
+     
+     for(int i=0;i<n;i++){
+         if(prices[i]<mini){
+             mini = prices[i];
+         }
+         
+         else if((prices[i]-mini) > profit){
+             profit = prices[i] - mini;
+         }
+     }
+        
+        return profit;
+    }
+};
